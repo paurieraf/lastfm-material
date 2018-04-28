43b4a248f0a69d2baa868eaea4bebead804fcf94
Pau Riera <paurf3@gmail.com>
2018-04-28 14:11:48 +0200

    Testing with react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';'
+import { Route, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './App.css';
